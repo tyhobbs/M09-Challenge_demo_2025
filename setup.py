6453731b851e2a2f7be6fcedf56bcc33fef3b2ae
vsg3se <vsg3se@virginia.edu>
2025-03-18 20:19:47 -0400

    Done.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/ontoligent/m09-demo',
+    author='Tyler Hobbs',
+    author_email='vsg3se@virginia.edu',
+    license='MIT',
+    packages=['M09-Challenge_demo_2025']
+)
